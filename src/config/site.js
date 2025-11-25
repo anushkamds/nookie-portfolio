a8d12fc0f73f79801f1cca4929528c736702f6f5
Ansuhka Mahesh <maheshdesilva89@gmail.com>
2025-11-25 12:13:39 +0000

    update OG image styles and met data

--- src/config/site.js
+++ src/config/site.js
@@ -1,56 +1,57 @@
 // Get site URL from environment variable, use default value if not set
 // Note: Please set the correct PUBLIC_SITE_URL in .env file after first deployment
-const SITE_URL = import.meta.env.PUBLIC_SITE_URL || 'https://nookie.dev/';
+const SITE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5200"
+    : import.meta.env.PUBLIC_SITE_URL || "https://nookie.dev";
 
 export const siteConfig = {
-	title: "Anushka Mahesh",
-	author: "Anushka",
-	url: SITE_URL,
-	mail: "hello@ricoui.com",
-	// resume add your resume file path here: /assets/resume.pdf
-	resume: "./Anushka_Mahesh-Fullstack_Engineer-Fed_2025.pdf",
-	utm: {
-		source: `${SITE_URL}`,
-		medium: "referral",
-		campaign: "navigation",
-	},
-	meta: {
-		title: "Ricoui Portfolio",
-		description: "I'm Rico, a web designer passionate about both design and code. Currently developing a personal product for the design community.",
-		keywords: "web designer, portfolio, design, code, personal website",
-		image: `${SITE_URL}/og.jpg`,
-		twitterHandle: "ricouii",
-	},
-	// social links
-	social: {
-		twitter: "https://x.com/ricouii",
-		twitterName: "ricouii",
-		github: "https://github.com/anushkamds",
-		blog: "https://nookie.dev/",
-		xiaohongshu: "https://www.xiaohongshu.com/user/profile/5f2b6903000000000101f51f"
-	},
+  title: "nookie.dev",
+  author: "Anushka Mahesh",
+  url: SITE_URL,
+  mail: "anushka@nookie.dev",
+  // resume add your resume file path here: /assets/resume.pdf
+  resume: "./Anushka_Mahesh-Fullstack_Engineer-Fed_2025.pdf",
+  utm: {
+    source: `${SITE_URL}`,
+    medium: "referral",
+    campaign: "navigation",
+  },
+  meta: {
+    title: "ANushka Portfolio",
+    description:
+      "I'm Anushka, a web designer passionate about both design and code. Currently developing a personal product for the design community.",
+    keywords:
+      "web designer, portfolio, design, code, personal website,fullstack developer,devops",
+    image: `${SITE_URL}/og.jpg`,
+    twitterHandle: "ricouii",
+  },
+  // social links
+  social: {
+    twitter: "https://x.com/ricouii",
+    twitterName: "ricouii",
+    github: "https://github.com/anushkamds",
+    blog: "https://nookie.dev/",
+    xiaohongshu:
+      "https://www.xiaohongshu.com/user/profile/5f2b6903000000000101f51f",
+  },
 };
 
 // Footer
 export const socialLinks = [
-
-	{
-		name: 'Github',
-		url: 'https://github.com/anushkamds',
-		icon: `<svg t="1730125604816" class="icon ic-github ic-social" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12741" width="256" height="256"><path d="M511.957333 21.333333C241.024 21.333333 21.333333 240.981333 21.333333 512c0 216.832 140.544 400.725333 335.573334 465.664 24.490667 4.394667 32.256-10.069333 32.256-23.082667 0-11.690667 0.256-44.245333 0-85.205333-136.448 29.610667-164.736-64.64-164.736-64.64-22.314667-56.704-54.4-71.765333-54.4-71.765333-44.586667-30.464 3.285333-29.824 3.285333-29.824 49.194667 3.413333 75.178667 50.517333 75.178667 50.517333 43.776 75.008 114.816 53.333333 142.762666 40.789333 4.522667-31.658667 17.152-53.376 31.189334-65.536-108.970667-12.458667-223.488-54.485333-223.488-242.602666 0-53.546667 19.114667-97.322667 50.517333-131.669334-5.034667-12.330667-21.930667-62.293333 4.778667-129.834666 0 0 41.258667-13.184 134.912 50.346666a469.802667 469.802667 0 0 1 122.88-16.554666c41.642667 0.213333 83.626667 5.632 122.88 16.554666 93.653333-63.488 134.784-50.346667 134.784-50.346666 26.752 67.541333 9.898667 117.504 4.864 129.834666 31.402667 34.346667 50.474667 78.122667 50.474666 131.669334 0 188.586667-114.730667 230.016-224.042666 242.090666 17.578667 15.232 33.578667 44.672 33.578666 90.453334v135.850666c0 13.141333 7.936 27.605333 32.853334 22.869334C862.250667 912.597333 1002.666667 728.746667 1002.666667 512 1002.666667 240.981333 783.018667 21.333333 511.957333 21.333333z" p-id="12742"></path></svg>`
-	},
-	{
-		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/anushkamahesh/',
-		icon: '<svg  class="icon ic-linkedin ic-social" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512" xml:space="preserve"><path d="M-143,145v512h512V145H-143z M41.4,508.1H-8.5V348.4h49.9V508.1z M15.1,328.4h-0.4c-18.1,0-29.8-12.2-29.8-27.7 c0-15.8,12.1-27.7,30.5-27.7c18.4,0,29.7,11.9,30.1,27.7C45.6,316.1,33.9,328.4,15.1,328.4z M241,508.1h-56.6v-82.6 c0-21.6-8.8-36.4-28.3-36.4c-14.9,0-23.2,10-27,19.6c-1.4,3.4-1.2,8.2-1.2,13.1v86.3H71.8c0,0,0.7-146.4,0-159.7h56.1v25.1 c3.3-11,21.2-26.6,49.8-26.6c35.5,0,63.3,23,63.3,72.4V508.1z"/></svg>'
-	},
-	{
-		name: 'RSS',
-		url: '/rss.xml',
-		icon: `<svg t="1730123988138" class="icon ic-rss ic-social " viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11766" width="256" height="256"><path d="M329.143 768q0 45.714-32 77.714t-77.714 32-77.715-32-32-77.714 32-77.714 77.715-32 77.714 32 32 77.714z m292.571 70.286q1.143 16-9.714 27.428-10.286 12-26.857 12H508q-14.286 0-24.571-9.428T472 844.57q-12.571-130.857-105.429-223.714T142.857 515.43q-14.286-1.143-23.714-11.429t-9.429-24.571v-77.143q0-16.572 12-26.857 9.715-9.715 24.572-9.715h2.857q91.428 7.429 174.857 46T472 515.43q65.143 64.571 103.714 148t46 174.857z m292.572 1.143q1.143 15.428-10.286 26.857-10.286 11.428-26.286 11.428H796q-14.857 0-25.429-10T759.43 843.43Q752.57 720.57 701.714 610T569.43 418t-192-132.286T144 227.43q-14.286-0.572-24.286-11.143t-10-24.857v-81.715q0-16 11.429-26.285 10.286-10.286 25.143-10.286H148q149.714 7.428 286.571 68.571t243.143 168q106.857 106.286 168 243.143t68.572 286.572z" p-id="11767"></path></svg>`
-	},
-
-
+  {
+    name: "Github",
+    url: "https://github.com/anushkamds",
+    icon: `<svg t="1730125604816" class="icon ic-github ic-social" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12741" width="256" height="256"><path d="M511.957333 21.333333C241.024 21.333333 21.333333 240.981333 21.333333 512c0 216.832 140.544 400.725333 335.573334 465.664 24.490667 4.394667 32.256-10.069333 32.256-23.082667 0-11.690667 0.256-44.245333 0-85.205333-136.448 29.610667-164.736-64.64-164.736-64.64-22.314667-56.704-54.4-71.765333-54.4-71.765333-44.586667-30.464 3.285333-29.824 3.285333-29.824 49.194667 3.413333 75.178667 50.517333 75.178667 50.517333 43.776 75.008 114.816 53.333333 142.762666 40.789333 4.522667-31.658667 17.152-53.376 31.189334-65.536-108.970667-12.458667-223.488-54.485333-223.488-242.602666 0-53.546667 19.114667-97.322667 50.517333-131.669334-5.034667-12.330667-21.930667-62.293333 4.778667-129.834666 0 0 41.258667-13.184 134.912 50.346666a469.802667 469.802667 0 0 1 122.88-16.554666c41.642667 0.213333 83.626667 5.632 122.88 16.554666 93.653333-63.488 134.784-50.346667 134.784-50.346666 26.752 67.541333 9.898667 117.504 4.864 129.834666 31.402667 34.346667 50.474667 78.122667 50.474666 131.669334 0 188.586667-114.730667 230.016-224.042666 242.090666 17.578667 15.232 33.578667 44.672 33.578666 90.453334v135.850666c0 13.141333 7.936 27.605333 32.853334 22.869334C862.250667 912.597333 1002.666667 728.746667 1002.666667 512 1002.666667 240.981333 783.018667 21.333333 511.957333 21.333333z" p-id="12742"></path></svg>`,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/anushkamahesh/",
+    icon: '<svg  class="icon ic-linkedin ic-social" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512" xml:space="preserve"><path d="M-143,145v512h512V145H-143z M41.4,508.1H-8.5V348.4h49.9V508.1z M15.1,328.4h-0.4c-18.1,0-29.8-12.2-29.8-27.7 c0-15.8,12.1-27.7,30.5-27.7c18.4,0,29.7,11.9,30.1,27.7C45.6,316.1,33.9,328.4,15.1,328.4z M241,508.1h-56.6v-82.6 c0-21.6-8.8-36.4-28.3-36.4c-14.9,0-23.2,10-27,19.6c-1.4,3.4-1.2,8.2-1.2,13.1v86.3H71.8c0,0,0.7-146.4,0-159.7h56.1v25.1 c3.3-11,21.2-26.6,49.8-26.6c35.5,0,63.3,23,63.3,72.4V508.1z"/></svg>',
+  },
+  {
+    name: "RSS",
+    url: "/rss.xml",
+    icon: `<svg t="1730123988138" class="icon ic-rss ic-social " viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11766" width="256" height="256"><path d="M329.143 768q0 45.714-32 77.714t-77.714 32-77.715-32-32-77.714 32-77.714 77.715-32 77.714 32 32 77.714z m292.571 70.286q1.143 16-9.714 27.428-10.286 12-26.857 12H508q-14.286 0-24.571-9.428T472 844.57q-12.571-130.857-105.429-223.714T142.857 515.43q-14.286-1.143-23.714-11.429t-9.429-24.571v-77.143q0-16.572 12-26.857 9.715-9.715 24.572-9.715h2.857q91.428 7.429 174.857 46T472 515.43q65.143 64.571 103.714 148t46 174.857z m292.572 1.143q1.143 15.428-10.286 26.857-10.286 11.428-26.286 11.428H796q-14.857 0-25.429-10T759.43 843.43Q752.57 720.57 701.714 610T569.43 418t-192-132.286T144 227.43q-14.286-0.572-24.286-11.143t-10-24.857v-81.715q0-16 11.429-26.285 10.286-10.286 25.143-10.286H148q149.714 7.428 286.571 68.571t243.143 168q106.857 106.286 168 243.143t68.572 286.572z" p-id="11767"></path></svg>`,
+  },
 ];
-
-
---
+++ src/pages/blog/[slug]/og.png.ts
@@ -0,0 +1,168 @@
+import { siteConfig } from "@/config/site.js";
+import { getCollection, type CollectionEntry } from "astro:content";
+import fs from "fs";
+import path from "path";
+import { ImageResponse } from "@vercel/og";
+
+interface Props {
+  params: { slug: string };
+  props: { post: CollectionEntry<"post"> };
+}
+
+export async function GET({ props, params }: Props) {
+  const { post } = props;
+  const { slug } = params;
+
+  if (!slug) {
+    return new Response("Slug parameter is required", { status: 400 });
+  }
+
+  // Return 404 if the post doesn't exist
+  if (!post) {
+    return new Response("Blog post not found", { status: 404 });
+  }
+
+  const postCover = fs.readFileSync("./public/favicon.png");
+
+  // Astro doesn't support tsx endpoints so usign React-element objects
+  const html = {
+    type: "div",
+    props: {
+      children: [
+        {
+          type: "div",
+          props: {
+            tw: "pl-8 shrink flex text-shadow-lg/30",
+            children: [
+              {
+                type: "div",
+                props: {
+                  style: {
+                    fontSize: "48px",
+                    fontFamily: "DM Mono Bold",
+                    backgroundImage:
+                      "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 21))",
+                    backgroundClip: "text",
+                    "-webkit-background-clip": "text",
+                    color: "transparent",
+                  },
+                  children: post.data.title,
+                },
+              },
+            ],
+          },
+        },
+        {
+          type: "div",
+          props: {
+            // using tailwind
+            tw: "absolute top-[20px] right-[20px] w-[120px] h-[120px] flex",
+            children: [
+              {
+                type: "img",
+                props: {
+                  src: postCover.buffer,
+                },
+              },
+            ],
+          },
+        },
+
+        {
+          type: "div",
+          props: {
+            tw: "absolute left-[40px] bottom-[40px] flex items-center",
+            children: [
+              {
+                type: "div",
+                props: {
+                  tw: "text-3xl float-left",
+                  style: {
+                    fontFamily: "DM Mono Bold",
+                  },
+                  children: post.data.publishDate.toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  }),
+                },
+              },
+            ],
+          },
+        },
+        {
+          type: "div",
+          props: {
+            tw: "absolute right-[40px] bottom-[40px] flex items-center",
+            children: [
+              {
+                type: "div",
+                props: {
+                  tw: "text-3xl float-left",
+                  style: {
+                    fontFamily: "Space Grotesk Regular",
+                  },
+                  children: siteConfig.author,
+                },
+              },
+              {
+                type: "div",
+                props: {
+                  tw: "px-2 text-3xl",
+                  style: {
+                    fontSize: "30px",
+                  },
+                  children: "|",
+                },
+              },
+              {
+                type: "div",
+                props: {
+                  tw: "text-3xl",
+                  children: siteConfig.title,
+                },
+              },
+            ],
+          },
+        },
+      ],
+      tw: "w-full h-full flex items-center justify-center relative px-22",
+      style: {
+        background: "#f7f8e8",
+        backgroundImage:
+          "radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)",
+        backgroundSize: "100px 100px",
+      },
+    },
+  };
+
+  return new ImageResponse(html, {
+    width: 1200,
+    height: 600,
+    fonts: [
+      {
+        name: "DM Mono Bold",
+        data: await fs.readFileSync("./src/assets/fonts/DMMono-Medium.ttf"),
+        style: "normal",
+        weight: 700,
+      },
+      {
+        name: "Space Grotesk Regular",
+        data: await fs.readFileSync(
+          "./src/assets/fonts/SpaceGrotesk-Regular.otf"
+        ),
+        style: "normal",
+        weight: 400,
+      },
+    ],
+  });
+}
+
+// to generate an image for each blog posts in a collection
+export async function getStaticPaths() {
+  const blogPosts = await getCollection("post");
+  return blogPosts.map((post) => ({
+    params: { slug: post.id },
+    props: { post },
+  }));
+}
